bda51f4755fce18197a5faae523bf23c962438a2
Arjun A <arjunajayan2005@gmail.com>
2024-07-21 13:55:53 +0530

    email v8

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,42 +1,42 @@
-const emailjs = require('@emailjs/browser'); 
+const emailjs = require('@emailjs/nodejs'); 
 
 exports.handler = async (event, context) => {
   try {
     const { firstName, lastName, gender, phoneNumber, message, email } = JSON.parse(event.body);
 
-    global.location = { protocol: 'https:' }; 
-    
     emailjs.init({
-      publicKey: process.env.EMAILJS_PUBLIC_KEY,
+      serviceId: process.env.EMAILJS_SERVICE_ID, 
+      templateId: process.env.EMAILJS_TEMPLATE_ID,
+      publicKey: process.env.EMAILJS_PUBLIC_KEY 
     });
 
     const templateParams = {
-      firstName: firstName,
-      lastName: lastName,
-      gender: gender,
-      phoneNumber: phoneNumber,
-      message: message,
-      to_email: email, 
+      firstName,
+      lastName,
+      gender,
+      phoneNumber,
+      message,
+      to_email: email
     };
 
     const response = await emailjs.send(
-      process.env.EMAILJS_SERVICE_ID, 
-      process.env.EMAILJS_TEMPLATE_ID, 
+      process.env.EMAILJS_SERVICE_ID,
+      process.env.EMAILJS_TEMPLATE_ID,
       templateParams
     );
 
-    console.log('Email sent successfully!', response.status, response.text);
+    console.log('Email sent successfully:', response);
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully!' }),
+      body: JSON.stringify({ message: 'Email sent successfully!' })
     };
 
   } catch (error) {
     console.error('Error sending email:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Error sending email.' }),
+      body: JSON.stringify({ message: 'Error sending email.' })
     };
   }
 };
